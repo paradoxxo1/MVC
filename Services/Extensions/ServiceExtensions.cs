323520626ad8842c13c7a590a2d9b49dde281714
Mahmut DEMİRKIRAN <124463263+paradoxxo1@users.noreply.github.com>
2026-01-31 23:06:22 +0300

    best practices

--- Services/Extensions/ServiceExtensions.cs
+++ Services/Extensions/ServiceExtensions.cs
@@ -3,6 +3,7 @@ using App.Services.ExceptionHandler;
 using App.Services.Products;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
@@ -12,6 +13,8 @@ public static class ServiceExtensions
 {
     public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
     {
+        services.Configure<ApiBehaviorOptions>(options => options.SuppressModelStateInvalidFilter = true);
+
         services.AddScoped<IProductService, ProductService>();
         services.AddScoped<ICategoryService, CategoryService>();
         services.AddFluentValidationAutoValidation();
